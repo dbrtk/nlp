c4688bae8b41fbe295a79dc5b23ad376f63ed05a
Dominik Bartkowski <dominik@dbrtk.net>
2017-01-26 14:53:57 +0100

    working on clustering

--- nlp/clustering/clusters.py
+++ nlp/clustering/clusters.py
@@ -194,6 +194,66 @@ def rotatematrix(data):
     return newdata
 
 
+def kcluster(rows, distance=pearson, k=4):
+    # Determine the minimum and maximum values for each point
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+    # Create k randomly placed centroids
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(k)]
+    lastmatches = None
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches = [[] for i in range(k)]
+        # Find which centroid is the closest for each row
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row):
+                    bestmatch = i
+            bestmatches[bestmatch].append(j)
+        # If the results are the same as last time, this is complete
+        if bestmatches == lastmatches:
+            break
+        lastmatches = bestmatches
+
+        # Move the centroids to the average of their members
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+
+    return bestmatches
+
+
+def print_clusters(kclust, rows):
+    """Printing the clusters."""
+    for k in kclust:
+        print('\n')
+        print([rows[_] for _ in k])
+
+
+def tanimoto(v1, v2):
+    """
+    """
+    c1, c2, shr = 0, 0, 0
+    for i in range(len(v1)):
+        if v1[i] != 0:
+            c1 += 1  # in v1
+        if v2[i] != 0:
+            c2 += 1  # in v2
+        if v1[i] != 0 and v2[i] != 0:
+            shr += 1  # in both
+    return 1.0 - (float(shr) / (c1 + c2 - shr))
+
+
 def main():
 
     # blognames,words,data
@@ -203,9 +263,17 @@ def main():
     # printclust(clust, labels=rows)
     # drawdendrogram(clust, rows, jpeg='blogclust.jpg')
 
-    rdata = rotatematrix(data)
-    wordclust = hcluster(rdata)
-    drawdendrogram(wordclust, labels=cols, jpeg='wordclust.jpg')
+    # rdata = rotatematrix(data)
+    # wordclust = hcluster(rdata)
+    # drawdendrogram(wordclust, labels=cols, jpeg='wordclust.jpg')
+
+    # k-means clustering
+    # kclust = kcluster(data, k=10)
+    # print_clusters(kclust, rows)
+
+    wants, people, data = readfile('zebo.txt')
+    clust = hcluster(data, distance=tanimoto)
+    drawdendrogram(clust, wants)
 
 
 if __name__ == "__main__":
