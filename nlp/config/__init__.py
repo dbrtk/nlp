1fb163a5bcdd8c2147020d780f223ee8298cf438
Dominik Bartkowski <dominik@dbrtk.net>
2018-11-13 15:59:11 +0100

    -

--- nlp/config/__init__.py
+++ nlp/config/__init__.py
@@ -40,7 +40,7 @@ DATA_ROOT = '/data/nlpdata'
 
 # RSYNC_SCRIPTS_PATH = os.path.abspath('/opt/rmxbin')
 # These are small shell scripts from rmxbin.
-RSYNC_SCRIPTS_PATH = os.path.abspath('/app/rmxbin')
+RSYNC_SCRIPTS_PATH = os.path.abspath('/opt/rmxbin')
 
 RSYNC_GET_DATA = os.path.join(RSYNC_SCRIPTS_PATH, 'getdata.sh')
 RSYNC_GET_VECTORS = os.path.join(RSYNC_SCRIPTS_PATH, 'getvect.sh')
