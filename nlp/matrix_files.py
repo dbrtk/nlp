0cd819afd87a1e7c5f308dbc05325df8b8cf235a
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2018-12-03 20:17:46 +0100

    getting rid of the methods that call rsync/ssh

--- nlp/matrix_files.py
+++ nlp/matrix_files.py
@@ -4,59 +4,31 @@
 """
 
 import os
-import shlex
 import shutil
-import subprocess
 
-from .config import (DATA_ROOT, PROXIMITYBOT_HOST_NAME, PROXIMITY_USER,
-                     RSYNC_GET_DATA, RSYNC_GET_VECTORS, RSYNC_POST_DATA)
-
-
-def sync_corpus_data(
-        remote_path: str = None,
-        unique_id: str = None,
-        get=True,
-        get_vectors=False,
-        **kwds):
-    """This function copies matrix files form the rmxbot server to the machine
-    that runs nlp; it uses rsync through ssh.
-
-    """
-    local_path = os.path.join(DATA_ROOT, unique_id)
-
-    if get_vectors:
-        script_path = RSYNC_GET_VECTORS
-    elif get:
-        script_path = RSYNC_GET_DATA
-    else:
-        script_path = RSYNC_POST_DATA
-
-    res = subprocess.run(
-        shlex.split(
-            "%(command)s %(path)s %(host)s %(user)s %(remote)s %(local)s" % {
-                'command': 'sh',
-                'path': script_path,
-                'host': PROXIMITYBOT_HOST_NAME,
-                'user': PROXIMITY_USER,
-                'remote': remote_path,
-                'local': local_path
-            }
-        ),
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-    if res.returncode == 0:
-        return local_path
-    raise RuntimeError(local_path)
+from .config import DATA_ROOT
 
 
 def unpack_corpus(tmp_upload_path: str = None, unique_id: str = None):
 
     path = os.path.join(DATA_ROOT, unique_id)
     shutil.unpack_archive(tmp_upload_path, path, 'zip')
+
+    # todo(): get rid of chmod!
+    # def chmod(path):
+    #     os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+    # chmod(path)
+    # for root, dirs, files in os.walk(path):
+    #     for f in files:
+    #         chmod(os.path.join(root, f))
+    #     for d in dirs:
+    #         chmod(os.path.join(root, d))
     return path
 
 
 def unpack_vectors(tmp_upload_path: str = None, unique_id: str = None):
-    pass
+
+    path = os.path.join(DATA_ROOT, unique_id)
+    shutil.unpack_archive(tmp_upload_path, path, 'zip')
+    return path
