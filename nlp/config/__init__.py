f836d9bf6765dba03a6367d4aef07e45e2976e48
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2018-12-03 22:48:38 +0100

    -

--- nlp/config/__init__.py
+++ nlp/config/__init__.py
@@ -1,12 +1,6 @@
 
 import os
 
-from django.conf import settings
-
-# PROXIMITY_BOT_PROJ is specific to the local deployment of proximity-bot.
-# todo(): delete in production
-PROXIMITY_BOT_PROJ = settings.PROJECT_DIR
-
 
 MAX_ITERATE = 100
 
@@ -15,7 +9,7 @@ DEFAULT_LANGUAGE = 'english'
 
 STOPWORD_REPLACEMENT = "___"
 
-# Defines whether proximity-bot with rmxbot are run on a remote server. If 
+# Defines whether proximity-bot with rmxbot are run on a remote server. If
 # set to True, proximity-bot should be accessed on a different machine
 PROXIMITYBOT_IS_REMOTE = False
 
@@ -35,8 +29,8 @@ CORPUS_COMPUTE_CALLBACK = '{}/'.format(_CORPUS_COMPUTE_CALLBACK)
 CORPUS_LEMMA_WORDS_PATH = 'lemma-words'
 
 # the place where nlp will store its temporary files; i.e. matrices, corpora.
-DATA_ROOT = os.path.join(PROXIMITY_BOT_PROJ, 'data', 'nlp')
+DATA_ROOT = '/data'
 
 # nltk confiigs
-# NLTK_DATA_PATH = '/opt/nltk_data'
-NLTK_DATA_PATH = os.path.join(DATA_ROOT, 'nltk_data')
+NLTK_DATA_PATH = '/opt/nltk_data'
+# NLTK_DATA_PATH = os.path.join(DATA_ROOT, 'nltk_data')
