3e2d090ae3af37b2dae68bd73d53f05af949acbf
Dominik Bartkowski <dominik.bartkowski@gmail.com>
2019-06-03 18:27:31 +0100

    -

--- nlp/config/appconf.py
+++ nlp/config/appconf.py
@@ -9,14 +9,13 @@ DEFAULT_LANGUAGE = 'english'
 
 STOPWORD_REPLACEMENT = "___"
 
-CORPUS_LEMMA_WORDS_PATH = 'lemma-words'
-
 # the place where nlp will store its temporary files; i.e. matrices, corpora.
 DATA_ROOT = os.environ.get('DATA_ROOT')
 UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
 
 # nltk confiigs
-
 NLTK_DATA_PATH = os.environ.get('NLTK_DATA_PATH')
 
+# celery configs
 CELERY_TIME_LIMIT = 1800
+
--- nlp/routes.py
+++ nlp/routes.py
@@ -1,23 +1,11 @@
 
 import json
-import os
-import uuid
 
 from flask import Blueprint, jsonify, request
 
-from . import matrix_files, task
-from .config.appconf import UPLOAD_FOLDER
 from .views import features_and_docs
 
 nlp_app = Blueprint('nlp_app', __name__, root_path='/')
-
-
-def matrix_update():
-    """Updating the matrices with new documents."""
-
-    # todo(): delete
-
-    pass
 
 
 @nlp_app.route('/features-docs/')
@@ -31,11 +19,3 @@ def get_features_and_docs():
         'features': features,
         'docs': docs
     })
-
-
-@nlp_app.route('/test-celery/')
-def test_celery():
-
-    res = task.test_task.apply_async(args=[1, 22345345]).get()
-
-    return jsonify({'success': True, 'result': res})
